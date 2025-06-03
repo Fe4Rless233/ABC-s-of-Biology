b6adc0223064739a8848d44c87cbbc09b1a24c84
ReadingCookiesA <92818312+Fe4Rless233@users.noreply.github.com>
2025-06-02 23:04:17 -0400

    Added Images

--- data.js
+++ data.js
@@ -3,208 +3,208 @@ const biologyABCData = [
     letter: "A",
     term: "Analogous structures",
     imageSrc: "images/A.jpg",
-    definition: "[Your definition for Analogous structures in your own words...]",
-    explanation: "[Explanation of how your ORIGINAL NATURAL photo illustrates Analogous structures...]",
-    unit: "[Unit for Analogous structures...]"
+    definition: "Analogous structures are certain organs between different species that have the same function, but the two species do not have a common ancestor",
+    explanation: "	The human arm is a great example of a analogous structure when compared to a octopus tentacle. Both are used for grasping and holding objects yet Octupi and humans share no common ancestors",
+    unit: "Unit 7: Theory of Evolution"
   },
   {
     letter: "B",
     term: "Biotic Factor",
     imageSrc: "images/B.jpg",
-    definition: "[Your definition for Biotic Factor in your own words...]",
-    explanation: "[Explanation of how your ORIGINAL NATURAL photo illustrates Biotic Factor...]",
-    unit: "[Unit for Biotic Factor...]"
+    definition: "A biotic factor is any living organism that has an impact on the ecosystem as a whole.",
+    explanation: "A tree is an excellent example of a biotic factor has it is a living thing that provides a benefit such as food, shelter for animals, and plays an important role in the overall carbon cycle",
+    unit: "OpenSci Ed Unit 1: Ecosystems"
   },
   {
     letter: "C",
     term: "Covalent Bonds",
     imageSrc: "images/C.jpg",
-    definition: "[Your definition for Covalent Bonds in your own words...]",
-    explanation: "[Explanation of how your ORIGINAL NATURAL photo illustrates Covalent Bonds...]",
-    unit: "[Unit for Covalent Bonds...]"
+    definition: "A covalent bond is a chemical bond formed where two atoms share one or more pairs of electrons.",
+    explanation: "A bowl of water shows covalent bonds because the water molecules within it are formed by oxygen and hydrogen atoms sharing electrons.",
+    unit: "Unit 2: Chemistry of Life"
   },
   {
     letter: "D",
     term: "DNA Ligase",
     imageSrc: "images/D.jpg",
-    definition: "[Your definition for DNA Ligase in your own words...]",
-    explanation: "[Explanation of how your ORIGINAL NATURAL photo/analogy illustrates DNA Ligase...]",
-    unit: "[Unit for DNA Ligase...]"
+    definition: "DNA ligase is an enzyme that serves as a connector between Okazaki Fragments, which are create due to the directionality of DNA polymerase",
+    explanation: "Since DNA ligase serves as connector between two objects, We believe that glue would be an excellent parallel as both connect and combine objects/materials.",
+    unit: "Unit 5: DNA & Heredity"
   },
   {
     letter: "E",
     term: "Enzyme",
     imageSrc: "images/E.jpg",
-    definition: "[Your definition for Enzyme in your own words...]",
-    explanation: "[Explanation of how your ORIGINAL NATURAL photo illustrates Enzyme...]",
-    unit: "[Unit for Enzyme...]"
+    definition: "Enzymes are certain proteins that speed up reactions. Their primary purpose is lowering the energy required to perform a certain task",
+    explanation: "	The door knob represent how enzymes react with their substrate. Enzymes operates using a model where each enzyme is individually encoded to a certain substrate. The model the enzyme utilizes is known as the Lock and Key model.",
+    unit: "Unit 2: Chemistry of Life"
   },
   {
     letter: "F",
     term: "Facilitated diffusion",
     imageSrc: "images/F.jpg",
-    definition: "[Your definition for Facilitated diffusion in your own words...]",
-    explanation: "[Explanation of how your ORIGINAL NATURAL photo/analogy illustrates Facilitated diffusion...]",
-    unit: "[Unit for Facilitated diffusion...]"
+    definition: "Facilitated Diffusion is a form of a passive transport where molecules that cannot pass through the permeable membrane can go into the cell through channel and carrier proteins.",
+    explanation: "During the process of Photosynthesis, An enzyme known as ATP synthase also functions a channel protein for hydrogen ions to pass through.",
+    unit: "Unit 3: Cells"
   },
   {
     letter: "G",
     term: "Genetic variation",
     imageSrc: "images/G.jpg",
-    definition: "[Your definition for Genetic variation in your own words...]",
-    explanation: "[Explanation of how your ORIGINAL NATURAL photo illustrates Genetic variation...]",
-    unit: "[Unit for Genetic variation...]"
+    definition: "G - Genetic variation discusses how individuals within a population will have different nucleotide sequences, causing the individuals to present different phenotypes or possibility differences in immune system.",
+    explanation: "	Here we have two individuals who both present gene variation. The most apparent variation comes in the form of their melanin content in their skin. Also differences in eye color are apparent.",
+    unit: "Unit 6: Genetics"
   },
   {
     letter: "H",
     term: "Homeostasis",
-    imageSrc: "images/H.jpg",
-    definition: "[Your definition for Homeostasis in your own words...]",
-    explanation: "[Explanation of how your ORIGINAL NATURAL photo illustrates Homeostasis...]",
-    unit: "[Unit for Homeostasis...]"
+    imageSrc: "images/H.png",
+    definition: "Homeostasis is the body’s ability to maintain a stable internal environment despite the changes in the external environment.",
+    explanation: "In this picture it illustrates a man shivering where his body is trying to maintain homeostasis. If his body drops due to a cold environment, shivering is a response to bring back the temperature to normal by generating heat. ",
+    unit: "Unit 4: Bioenergetics"
   },
   {
     letter: "I",
-    term: "Independent variable",
+    term: "Interphase",
     imageSrc: "images/I.jpg",
-    definition: "[Your definition for Independent variable in your own words...]",
-    explanation: "[Explanation of how your ORIGINAL NATURAL photo illustrates an Independent variable...]",
-    unit: "[Unit for Independent variable...]"
+    definition: "A cell goes through interphase in order to grow to its full size and replicate its DNA before going through mitosis. ",
+    explanation: "Within Interphase, the cell goes through several checkpoints in order to confirm proper growth. If it passes the checkpoints, it will continue onto mitosis. A board game such as Life also contains checkpoints that restrict the player from passing forward unless requirements are met",
+    unit: "Unit 3: Cells"
   },
   {
     letter: "J",
     term: "James Watson",
     imageSrc: "images/J.jpg",
-    definition: "[Your definition for your 'J' term in your own words...]",
-    explanation: "[Explanation of how your ORIGINAL NATURAL photo illustrates your 'J' term...]",
-    unit: "[Unit for your 'J' term...]"
+    definition: "James Watson was an excellent scientist who managed to discover the double-helix structure of DNA",
+    explanation: "The picture shown is a picture known as Photograph 51. Photograph 51 was originally taken by Rosalind Franklin who should been credited for the discovery; however, James and other scientist stole the photograph and made their own conclusions without her, causing her not to be credited ",
+    unit: "Unit 5: DNA & Heredity"
   },
   {
     letter: "K",
     term: "Krebs cycle",
     imageSrc: "images/K.jpg",
-    definition: "[Your definition for Krebs cycle in your own words...]",
-    explanation: "[Explanation of how your ORIGINAL NATURAL photo illustrates Krebs cycle...]",
-    unit: "[Unit for Krebs cycle...]"
+    definition: "The Krebs cycle occurs during cellular respiration and produces ATP as well as NADH and FADH2 for the electron transport chain’s next step",
+    explanation: "Lemon Juice contains a molecule known as citric acid, which plays a important role within the Krebs cycle. Its ionized form, citrate, forms from the Acetyl-CoA during the cycle. Additionally, another name for the Krebs cycle is the Citric Acid Cycle",
+    unit: "Unit 4: Bioenergetics"
   },
   {
     letter: "L",
     term: "Lactic acid fermentation",
     imageSrc: "images/L.jpg",
-    definition: "[Your definition for Lactic acid fermentation in your own words...]",
-    explanation: "[Explanation of how your ORIGINAL NATURAL photo illustrates Lactic acid fermentation...]",
-    unit: "[Unit for Lactic acid fermentation...]"
+    definition: "Lactic Acid fermentation is anaerobic process in muscle cells fatigues muscles when there is a lack of oxygen, but need for energy",
+    explanation: "	The picture of the tennis player represents the activities that a person can go through that causes lactic acid to build up. As he continues to play more and struggles intaking oxygen, the muscle cells will begin to fatigue to the quantity of lactic acid. ",
+    unit: "Unit 4: Bioenergetics"
   },
   {
     letter: "M",
     term: "Mitosis",
-    imageSrc: "images/M.jpg",
-    definition: "[Your definition for Mitosis in your own words...]",
-    explanation: "[Explanation of how your ORIGINAL NATURAL photo illustrates Mitosis...]",
-    unit: "[Unit for Mitosis...]"
+    imageSrc: "images/M.jpg", 
+    definition: "A type of cell division that produces two haploids from one single cell",
+    explanation: "Skin cells are an excellent example of a reason for mitosis to exist. For example, after a cell has gone through Apoptosis or programmed cell death, the cells surrounding it will go through mitosis to replace that dead cell with a healthy, operational cell.",
+    unit: "Unit 3: Cells"
   },
   {
     letter: "N",
     term: "Nitrogen bases",
     imageSrc: "images/N.jpg",
-    definition: "[Your definition for Nitrogen bases in your own words...]",
-    explanation: "[Explanation of how your ORIGINAL NATURAL photo/analogy illustrates Nitrogen bases...]",
-    unit: "[Unit for Nitrogen bases...]"
+    definition: "Nitrogen bases are organic molecules that play a significant role in DNA and RNA processes",
+    explanation: "Since when DNA is flattened, it appears to look a ladder. The rungs of said ladder represent the places where the nitrogen bases would appear inside of DNA. 	",
+    unit: "Unit 5: DNA & Heredity"
   },
   {
     letter: "O",
     term: "Osmosis",
     imageSrc: "images/O.jpg",
-    definition: "[Your definition for Osmosis in your own words...]",
-    explanation: "[Explanation of how your ORIGINAL NATURAL photo illustrates Osmosis...]",
-    unit: "[Unit for Osmosis...]"
+    definition: "Osmosis is the movement of water molecules across a selectively permeable membrane along the concentration gradient (passive).",
+    explanation: "When a plant cell is in an environment where it takes in water through osmosis, its vacoule swells, pushing the contents against the rigid cell wall, ultimately creating turgid pressure that keeps the plant straight. This plant is an example of osmosis because it shows how water moves into the cell, causing it to swell and maintain its structure.",
+    unit: "Unit 3: Cells"
   },
   {
     letter: "P",
     term: "Polypeptide",
     imageSrc: "images/P.jpg",
-    definition: "[Your definition for Polypeptide in your own words...]",
-    explanation: "[Explanation of how your ORIGINAL NATURAL photo illustrates Polypeptide...]",
-    unit: "[Unit for Polypeptide...]"
+    definition: "A polypeptide is a long chain of amino acids that are chained together by peptide bonds.",
+    explanation: "Chicken nuggets are made of protein and proteins are made up of many polypeptides. When you eat a chicken nugget your body breaks down these polypeptides into amino acids to build and repair its own tissues.",
+    unit: "Unit 2: Chemistry of Life" 
   },
   {
     letter: "Q",
     term: "Quaternary structure",
     imageSrc: "images/Q.jpg",
-    definition: "[Your definition for Quaternary structure in your own words...]",
-    explanation: "[Explanation of how your ORIGINAL NATURAL photo/analogy illustrates Quaternary structure...]",
-    unit: "[Unit for Quaternary structure...]"
+    definition: "Quaternary structure refers to the complex structure formed when two or more polypeptide chains come together to form a functional protein.",
+    explanation: "	Here we have a picture of common indian snack known as maggi noodles. The structure and overall shape of these noodles are very similar to that of a protein within the quaternary structure, which shows its relevance to the term.",
+    unit: "Unit 2: Chemistry of Life"
   },
   {
     letter: "R",
     term: "Replication of DNA", 
     imageSrc: "images/R.jpg",
-    definition: "[Your definition for your 'R' term in your own words...]",
-    explanation: "[Explanation of how your ORIGINAL NATURAL photo/analogy illustrates your 'R' term...]",
-    unit: "[Unit for your 'R' term...]"
+    definition: "DNA replication is when a cell makes an identical copy of itself.",
+    explanation: "Here, the printer setup illustrates DNA replication. The original document on the scanner bed represents the parent DNA molecule. As the scanner reads it (like helicase unwinding DNA), the printing mechanism (acting like DNA polymerase) uses ink from cartridges (representing free nucleotides) to precisely recreate the document's information onto new sheets of paper, following the original's pattern (similar to base-pairing rules). The final stack of identical printed pages symbolizes the two new, identical DNA molecules produced, each a faithful copy of the original.",
+    unit: "Unit 5: DNA & Heredity"
   },
   {
     letter: "S",
     term: "Synapsis",
     imageSrc: "images/S.jpg",
-    definition: "[Your definition for Synapsis in your own words...]",
-    explanation: "[Explanation of how your ORIGINAL NATURAL photo/analogy illustrates Synapsis...]",
-    unit: "[Unit for Synapsis...]"
+    definition: "Synapsis is when homologous chromosomes cross over in prophase I.",
+    explanation: "Here, the two jeans illustrated, describe the jean legs intertwisting with one another, to present an accurate picture of the crossing over that occurs during prophase I.",
+    unit: "Unit 5: DNA & Heredity"
   },
   {
     letter: "T",
     term: "Trophic levels",
     imageSrc: "images/T.jpg",
-    definition: "[Your definition for Trophic levels in your own words...]",
-    explanation: "[Explanation of how your ORIGINAL NATURAL photo illustrates Trophic levels...]",
-    unit: "[Unit for Trophic levels...]"
+    definition: "Trophic levels describe the location of an organism in a food chain.",
+    explanation: "Here, the world famous google dinosaur represents an apex predator, the t-rex, that would be at a higher trophic level, than a toy triceratops, showing how organisms occupy different positions on a food chain. ",
+    unit: "OpenSci Ed Unit 1: Ecosystems"
   },
   {
     letter: "U",
     term: "Unicellular",
     imageSrc: "images/U.jpg",
-    definition: "[Your definition for Unicellular in your own words...]",
-    explanation: "[Explanation of how your ORIGINAL photo (e.g. hand for bacteria) illustrates Unicellular...]",
-    unit: "[Unit for Unicellular...]"
+    definition: "A unicellular organism is an organism that is made of only one single cell.",
+    explanation: "Here in this picture, while the hand itself is multicellular, it's covered in countless microscopic bacteria.",
+    unit: "Unit 3: Cells"
   },
   {
     letter: "V",
     term: "Vesicle Transport",
     imageSrc: "images/V.jpg",
-    definition: "[Your definition for Vesicle Transport in your own words...]",
-    explanation: "[Explanation of how your ORIGINAL NATURAL photo/analogy illustrates Vesicle Transport...]",
-    unit: "[Unit for Vesicle Transport...]"
+    definition: "Vesicle transport is a way that cells move larger substances across the cell membrane within the cell using vesicles.",
+    explanation: "The image of drugs shows how many medicines enter cells through vesicular transport, making sure they get to the right place within or outside the cell.",
+    unit: "Unit 3: Cells"
   },
   {
     letter: "W",
     term: "Water Cycle",
     imageSrc: "images/W.jpg",
-    definition: "[Your definition for Water Cycle in your own words...]",
-    explanation: "[Explanation of how your ORIGINAL NATURAL photo illustrates Water Cycle...]",
-    unit: "[Unit for Water Cycle...]"
+    definition: "The water cycle is the rotation of water through the Earth in evaporation, condensation, and precipitation.",
+    explanation: "The creek in the image shows part of this cycle representing the stage where water collects before evaporating back into the atmosphere.",
+    unit: "OpenSci Ed Unit 1: Ecosystems"
   },
   {
     letter: "X",
     term: "X-ray crystallography",
     imageSrc: "images/X.jpg",
-    definition: "[Your definition for X-ray crystallography in your own words...]",
-    explanation: "[Explanation of how your ORIGINAL NATURAL photo (e.g. crystal) illustrates X-ray crystallography...]",
-    unit: "[Unit for X-ray crystallography...]"
+    definition: "X-ray crystallography is the technique used to find structure of molecules, like proteins or DNA.",
+    explanation: "The photo of Rosalind Franklin a significant scientist who discovered the DNA’s double helix structure, which helped molecular biologists “see” molecules at their molecular level.",
+    unit: "Unit 5: DNA & Heredity"
   },
   {
     letter: "Y",
     term: "Y - Chromosome",
     imageSrc: "images/Y.jpg",
-    definition: "[Your definition for Y - Chromosome in your own words...]",
-    explanation: "[Explanation of how your ORIGINAL photo illustrates Y - Chromosome...]",
-    unit: "[Unit for Y - Chromosome...]"
+    definition: "he Y chromosome is one of the sex chromosomes in humans and is used to determine the male sex.",
+    explanation: "The male figure in the image represents the presence of a Y chromosome.",
+    unit: "Unit 6: Genetics"
   },
   {
     letter: "Z",
     term: "Zygote",
     imageSrc: "images/Z.jpg",
-    definition: "[Your definition for your 'Z' term in your own words...]",
-    explanation: "[Explanation of how your ORIGINAL NATURAL photo illustrates your 'Z' term...]",
-    unit: "[Unit for your 'Z' term...]"
+    definition: "A zygote is the single cell formed when a sperm fertilizes an egg",
+    explanation: "The chicken egg in the image shows a zygote. While not all chicken eggs are fertilized, a fertilized one begins the early stages of life. This image shows half of what is required to make a zygote. ",
+    unit: "Unit 6: Genetics"
   }
 ];
